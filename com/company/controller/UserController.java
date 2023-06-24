74ce4dfa5f9838cb8b18bec4a4b50d03009c7f42
JakubSzuba1993 <jakubszuba1@gmail.com>
2023-06-24 22:53:26 +0200

    Aktualizacja UserController.java
    Poprzednie "Commit summary" były wykonane wyłącznie testowo.

--- com/company/controller/UserController.java
+++ com/company/controller/UserController.java
@@ -4,7 +4,7 @@ import com.company.model.Address;
 
 public class UserController {
     public static void main(String[] args) {
-        Address address = new Address("Rzeszowska 100", "Lublin", "Polska");
+        Address address = new Address("Aleja Warszawska 100", "Lublin", "Polska");
         User user = new User("Jasiu Kowalski", 40, address);
         user.displayInfo();
     }
