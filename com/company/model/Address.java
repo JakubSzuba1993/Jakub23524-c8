560227539987d0d4869e2890f13b98d057453417
JakubSzuba1993 <jakubszuba1@gmail.com>
2023-06-24 23:26:17 +0200

    Aktualizacja stringów w klasie Address

--- com/company/model/Address.java
+++ com/company/model/Address.java
@@ -2,18 +2,18 @@ package com.company.model;
 
 public class Address {
     private String Ulica;
-    private String Miejscowosc;
+    private String MiejsceZamieszkania;
     private String Kraj;
 
-    public Address(String Ulica, String Miejscowosc, String Kraj) {
+    public Address(String Ulica, String MiejsceZamieszkania, String Kraj) {
         this.Ulica = Ulica;
-        this.Miejscowosc = Miejscowosc;
+        this.MiejsceZamieszkania = MiejsceZamieszkania;
         this.Kraj = Kraj;
     }
 
 
 
     public String getFullAddress() {
-        return Ulica + ", " + Miejscowosc + ", " + Kraj;
+        return Ulica + ", " + MiejsceZamieszkania + ", " + Kraj;
     }
 }
